16f930277a7e2bef99717394892ebd435699c459
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-02 13:10:22 -0300

    [PROJECT] - Seller entity class

--- src/main/java/model/entities/Department.java
+++ src/main/java/model/entities/Department.java
@@ -1,10 +1,12 @@
 package model.entities;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 public class Department implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Integer id;
